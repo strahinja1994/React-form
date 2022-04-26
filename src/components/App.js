a03b1472a0446f059cf6b5af83c17e760969da8b
strahinja1994 <82164543+strahinja1994@users.noreply.github.com>
2022-04-27 00:53:17 +0200

    Add files via upload

---
+++ src/components/App.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+export default function App() {
+
+    const [formData, setFormData] = React.useState(
+        {
+        email:"",
+        password : "",
+        confirm : "",
+        checkbox : true,
+        }
+    )
+
+    function handleChange(event) {
+        const {name, value, type, checked} = event.target;
+       setFormData((prevFormdata) => {
+            return{
+                ...prevFormdata,
+                [name] : type === "checkbox" ? checked : value
+            }
+       })
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log(formData);
+    }
+
+    return(
+        <div className="form__container">
+            <form className="form" onSubmit={handleSubmit}>
+                <input onChange={handleChange}
+                className="form__email form__input"
+                type="email"
+                placeholder="Email address"
+                name="email"
+                value={formData.email}
+                />
+                <input onChange={handleChange}
+                className="form__password form__input"
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={formData.password}
+                />
+                <input onChange={handleChange}
+                className="form__confirm form__input"
+                type="password"
+                placeholder="Confirm password"
+                name="confirm"
+                value={formData.confirm}
+                />
+                <div className="form__checker">
+                    <input onChange={handleChange}
+                    className="form__checker-checkbox form__input"
+                    id="checkbox"
+                    type="checkbox"
+                    name="checkbox"
+                    value={formData.checkbox}
+                    />
+                    <label  className="form__checker-label">I want to join the newsletter</label>
+                </div>
+                <button className="form__button">Sign up</button>
+            </form>
+        </div>
+    )
+}
